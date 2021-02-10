37115a0e53a11686c99823b6bf65f9b964456403
Alexey Shevchenko <kamikozz@mail.ru>
2021-02-10 05:27:09 +0300

    refactor: add eslint with no-es6, gitignore, editorconfig, sass, bootstrap

--- main.js
+++ main.js
@@ -1,58 +1,59 @@
-var todolist = (function () {
-	var b = 5;
+var todolist = (function _() {
+  var b = 5;
 
-	function handleA(event) {
+  function handleA(event) {
 
-	}
+  }
 
-	return {
-		b: b
-	};
+  return {
+    b: b,
+  };
 }());
 
 // var a = todolist.
 var tasksList = [];
 
 function changeTask() {
-	console.log('Changed Tasks properties!');
+  console.log('Changed Tasks properties!');
 }
 
 function changeStatus() {
-	console.log('ChangedTasksState');
+  console.log('ChangedTasksState');
 }
 
 function createNewTask(text, date, time) {
-	if (text.length !== 0 && date.length === 0 && time.length === 0) {
-		var newTask = Object.create(null);
-		newTask.text = text;
-		newTask.status = 'not done';
-		newTask.hasDeadline = false;
+  if (text.length !== 0 && date.length === 0 && time.length === 0) {
+    var newTask = Object.create(null);
+    newTask.text = text;
+    newTask.status = 'not done';
+    newTask.hasDeadline = false;
 
-		tasksList.push(newTask);
-		console.log('New task has been added!');
-		console.log(tasksList);
-	}
-	else if (text.length !== 0 && date.length !== 0 && time.length !== 0) {
-		var newTask = Object.create(null);
-		newTask.text = text;
-		newTask.status = 'not done';
-		newTask.hasDeadline = true;
-		newTask.date = date;
-		newTask.time = time;
+    tasksList.push(newTask);
+    console.log('New task has been added!');
+    console.log(tasksList);
+  }
+  else if (text.length !== 0 && date.length !== 0 && time.length !== 0) {
+    var newTask = Object.create(null);
+    newTask.text = text;
+    newTask.status = 'not done';
+    newTask.hasDeadline = true;
+    newTask.date = date;
+    newTask.time = time;
 
-		tasksList.push(newTask);
-		console.log('New task has been added!');
-		console.log(tasksList);
-	}
+    tasksList.push(newTask);
+    console.log('New task has been added!');
+    console.log(tasksList);
+  }
 }
 
-function deleteTask(idx = 2) {
-	// var idx = this.items.indexOf(item);
-	tasksList.splice(idx, 1);
-	console.log('\n has been deleted');
-	console.log(tasksList);
+function deleteTask(idx) {
+  var index = !idx ? 2 : idx;
+  // var idx = this.items.indexOf(item);
+  tasksList.splice(index, 1);
+  console.log('\n has been deleted');
+  console.log(tasksList);
 }
 
 function toggleEditMode() {
-	console.log('Edit mode has been activated');
+  console.log('Edit mode has been activated');
 }
